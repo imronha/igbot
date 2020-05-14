a4ebd286fb88523ce494f23d35f01743583f4c93
Imron Hajiahmad <imronha@gmail.com>
2020-05-13 22:35:55 -0700

    login function complete

--- ig.js
+++ ig.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer");
 
 const BASE_URL = "https://www.instagram.com/";
+const TAG_URL = (tag) => `https://www.instagram.com/explore/tags/${tag}/`;
 
 const ig = {
+  // Initialize puppeteer
   browser: null,
   page: null,
 
@@ -12,8 +14,31 @@ const ig = {
     });
 
     ig.page = await ig.browser.newPage();
+  },
 
+  // =====================================================
+  // Login function navigates to base url (instagram login page) and then enters user information and logs in
+  login: async (username, password) => {
     await ig.page.goto(BASE_URL, { waitUntil: "networkidle2" });
+
+    // Enter in username and password.
+    await ig.page.type('input[name="username"]', username, { delay: 500 });
+    await ig.page.type('input[name="password"]', password, { delay: 500 });
+
+    // Locate login button and then click
+    let loginButton = await ig.page.$x('//div[contains(text(), "Log In")]');
+    await loginButton[0].click();
+
+    // Wait until navigation is complete
+    await ig.page.waitForNavigation({ waitUntil: "networkidle2" });
+    console.log("Successfully logged in.");
+  },
+
+  // =====================================================
+  // likeTagsProcess function navigates to explore/tags/{tag} and likes the 3 most recent posts (not from top posts)
+  likeTagsProcess: async (tags = []) => {
+    for (let tag of tags) {
+    }
   },
 };
 
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const ig = require("./ig");
+require("dotenv").config();
+
+const username = process.env.IG_USERNAME;
+const password = process.env.IG_PASSWORD;
+console.log(username, password);
 
 (async () => {
   await ig.initialize();
+
+  await ig.login(username, password);
 
   debugger;
 })();
