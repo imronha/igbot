03ab696be863f1cd99bb93c8070d873bf2c658ac
Imron Hajiahmad <imronha@gmail.com>
2020-05-16 23:46:11 -0700

    liking kinda works

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 
 const username = process.env.IG_USERNAME;
 const password = process.env.IG_PASSWORD;
-console.log(username, password);
+// console.log(username, password);
 
 (async () => {
   await ig.initialize();
 
   await ig.login(username, password);
+
+  await ig.likeTagsProcess(["volleyballspikes", "volleyball"]);
 
   debugger;
 })();
